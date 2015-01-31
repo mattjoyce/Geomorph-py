d0e55bb90e8a2b76ed05aaec41513ebae6701052
Matt Joyce <matt.joyce@gmail.com>
2015-02-01 10:46:02 +1100

    basucally work, now playing with histograms

--- gen1.py
+++ gen1.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 import os
 import random
-import Image
+import Image, ImageEnhance
+import operator
+random.seed()
 
 class map:
-    def __init__(self, sizex=1, sizey=1, stroutfile):
+    def __init__(self, stroutfile,sizex=1, sizey=1):
         self.tx = sizex
         self.ty = sizey
         self.tilespath="/home/matt/projects/mapgen/tiles"
+        self.stroutfile=stroutfile
         self.corner=[]
         self.edge=[]
         self.base=[]
@@ -21,42 +24,104 @@ class map:
         tile=""
         if element == 0:  #top left corner
             tile=self.corner[random.randint(0,len(self.corner)-1)]
-            rotate=2
+            rotate=180
         elif element == 1:  #top centre edge
             tile=self.edge[random.randint(0,len(self.edge)-1)]
-            rotate=3
-        elif element == 2:  #top right
+            rotate=0
+        elif element == 2:  #top right corner
             tile=self.corner[random.randint(0,len(self.corner)-1)]
-            rotate=3
+            rotate=90
         elif element == 3:  # left edge
             tile=self.edge[random.randint(0,len(self.edge)-1)]
-            rotate=2
+            rotate=90
         elif element ==4:  # centre
             tile=self.base[random.randint(0,len(self.base)-1)]
-            rotate=random.randint(0,4);
+            rotate=random.randint(0,4)*90;
         elif element == 5:  # right edge
             tile=self.edge[random.randint(0,len(self.edge)-1)]
-            rotate=0
+            rotate=270
         elif element == 6:  # bottom left corner
             tile=self.corner[random.randint(0,len(self.corner)-1)]
-            rotate=0
+            rotate=270
         elif element == 7:  # bottom edge
             tile=self.edge[random.randint(0,len(self.edge)-1)]
-            rotate=1
+            rotate=180
         elif element == 8: #bottom right corner
             tile=self.corner[random.randint(0,len(self.corner)-1)]
             rotate=0
-        return (tile,rotate)
+        
+        im1=Image.open(self.tilespath+"/"+tile).rotate(rotate)
+        
+        # flip side edges and base vertically, maybe
+        if element in [3,4,5] and random.random()>0.5:
+            im2=im1.transpose(Image.FLIP_TOP_BOTTOM)
+        elif element in [1,4,7] and random.random()>0.5:
+            im2=im1.transpose(Image.FLIP_LEFT_RIGHT)
+        else:
+            im2=im1
+              
+        return(self.equalize(im2))
+        
     
     
     def build(self):
         #top row
-        strfile=self.tilespath+"/"+self.randtile(i)[0] #tl
-        im_element=Image.open(strfile)
-        self.im.paste(im,(0,0))
+        im_tile=self.randtile(0)
+        self.im.paste(im_tile,(0,0))
         
-        self.im.save(self.stroutfile)
+        for i in range(0,self.tx):
+            im_tile=self.randtile(1)
+            self.im.paste(im_tile,(250+(i*500),0))
+          
+        im_tile=self.randtile(2)
+        self.im.paste(im_tile,(250+(self.tx*500),0))
+        
+        # mid rows
+        for y in range(0,self.ty):
+            im_tile=self.randtile(3)
+            self.im.paste(im_tile,(0,250+(y*500)))
             
+            for x in range(0,self.tx):
+                im_tile=self.randtile(4)
+                self.im.paste(im_tile,(250+(x*500),250+(y*500)))
+        
+            im_tile=self.randtile(5)
+            self.im.paste(im_tile,(250+(self.tx*500),250+(y*500)))
+            
+            
+        #bottom
+        im_tile=self.randtile(6)
+        self.im.paste(im_tile,(0,250+(self.ty*500)))
+        
+        for i in range(0,self.tx):
+            im_tile=self.randtile(7)
+            self.im.paste(im_tile,(250+(i*500),250+(self.ty*500)))
+          
+        im_tile=self.randtile(8)
+        self.im.paste(im_tile,(250+(self.tx*500),250+(self.ty*500)))
+       
+        # filename=self.stroutfile[:-4]
+        # enhancer = ImageEnhance.Brightness(self.im)
+        # for k in range(1, 9):
+            # factor = k / 4.0
+            # print(factor),  # 0.25 0.5 0.75 1.0 1.25 1.5 1.75 2.0
+            # img_enhanced = enhancer.enhance(factor)
+            # img_enhanced.save(filename+"_%03d.jpg" % (int(factor*100)) )
+        self.im.save(self.stroutfile)
+
+    def equalize(self,im):
+        h = im.convert("L").histogram()
+        lut = []
+        for b in range(0, len(h), 256):
+            # step size
+            step = reduce(operator.add, h[b:b+256]) / 255
+            # create equalization lookup table
+            n = 0
+            for i in range(256):
+                lut.append(n / step)
+                n = n + h[i+b]
+        # map image through lookup table
+        return im.point(lut*3)     
         
     def loadtiles(self):
         os.chdir(self.tilespath)
@@ -68,6 +133,6 @@ class map:
             elif file.endswith(".jpg") and file.find("Base")>-1:
                 self.base.append(file)
 
-map1=map("/home/matt/projects/mapgen/test.jpg")
+map1=map("/home/matt/projects/mapgen/test.jpg",5,5)
 map1.loadtiles()
 map1.build()
